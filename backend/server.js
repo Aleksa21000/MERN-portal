a7013051311f08fe3c6c6d055d9e010b3af71f42
Aleksa21000 <radulovicaleksa21@gmail.com>
2025-02-27 00:13:49 +0100

    Created user model, singup controller done + generating token while creating user + hashing the password

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.js";
+import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // Middleware (be able to parse req.body)
+app.use(express.urlencoded({ extended: true })); // Middleware (be able to parse form data (urlended)
 
 app.use("/api/auth", authRoutes);
 
