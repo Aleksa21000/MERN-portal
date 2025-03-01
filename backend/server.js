e09d8f8f55cf48883de92ac661909f95ea854803
Aleksa21000 <radulovicaleksa21@gmail.com>
2025-03-01 11:50:18 +0100

    user routes & controllers in progress, created profile/username route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
@@ -16,6 +18,7 @@ app.use(express.urlencoded({ extended: true })); // Middleware (be able to parse
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
