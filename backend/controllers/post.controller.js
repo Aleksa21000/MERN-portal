394868e9f4af9b75d60a75705622f4df434d963e
Aleksa21000 <radulovicaleksa21@gmail.com>
2025-03-02 19:59:08 +0100

    added comment endpoint

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -55,3 +55,26 @@ export const deletePost = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+export const commentOnPost = async (req, res) => {
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id.toString();
+
+    if (!text) return res.status(400).json({ error: "Text field is required" });
+
+    const post = await Post.findById(postId);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    const comment = { user: userId, text };
+
+    post.comments.push(comment);
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (error) {
+    console.error("Error in commentOnPost:", error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
