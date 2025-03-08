6729540c90d9e6d52ce365ab7a4c40e19aa49da2
Aleksa21000 <radulovicaleksa21@gmail.com>
2025-03-08 18:43:04 +0100

    finished UI components&pages with dummy data, added basic Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
-function App() {
-  return (
-    <>
-      <h1 className="text-5xl text-white font-bold underline">Hello, App.jsx & Tailwind!</h1>
-      <button className="btn btn-primary m-5">Click me</button>
-      <button className="btn btn-secondary m-5">Click me</button>
-    </>
-  );
-}
+import { Route, Routes } from "react-router-dom";
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
+
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+const App = () => {
+    return (
+        <div className="flex max-w-6xl mx-auto">
+            <Sidebar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/notifications" element={<NotificationPage />} />
+                <Route path="/profile/:username" element={<ProfilePage />} />
+            </Routes>
+            <RightPanel />
+        </div>
+    );
+};
 
 export default App;
---
+++ frontend/src/components/svgs/LogoSvg.jsx
@@ -0,0 +1,10 @@
+const LogoSvg = (props) => (
+    <svg aria-hidden="true" viewBox="0 0 53 44" {...props}>
+        <path
+            d="M23.2997 0L52.0461 28.6301V44H38.6311V34.1553L17.7522 13.3607L13.415 13.3607L13.415 44H0L0 0L23.2997 0ZM38.6311 15.2694V0L52.0461 0V15.2694L38.6311 15.2694Z"
+            fill={props.fill}
+        />
+    </svg>
+);
+
+export default LogoSvg;
